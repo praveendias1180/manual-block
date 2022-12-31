ca210d0ffc2602ca742004d16959ff37a81d6a43
Praveen Dias <praveendias1180@gmail.com>
2022-12-31 10:11:21 +0530

    Scaffoloding done

--- manual-block.php
+++ manual-block.php
@@ -5,7 +5,7 @@
  */
 
  function manual_custom_block_script_register(){
-    wp_enqueue_script('manual-custom-block',plugin_dir_url(__FILE__).'manual-block.js');
+    wp_enqueue_script('manual-custom-block',plugin_dir_url(__FILE__).'manual-block.js',array('wp-blocks','wp-i18n','wp-editor'),true,false);
  }
 
  add_action('enqueue_block_editor_assets', 'manual_custom_block_script_register');
